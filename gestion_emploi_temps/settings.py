ab7e91ca621d868b43792967eb66b8acbe9684be
AcheikhYounous <younousaboukary.acheikh@gmail.com>
2026-06-28 16:54:12 +0100

    Ajout logo et icone du site

--- gestion_emploi_temps/settings.py
+++ gestion_emploi_temps/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+
+    "whitenoise.runserver_nostatic",
+
     'django.contrib.staticfiles',
 
 
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
